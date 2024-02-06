4f1578644be1ed2e146ff08c5b7be75b6eb12d02
Gustavo <gumarinho090306@gmail.com>
2024-02-06 20:05:13 -0300

    Add @studio-freight/lenis library and configure scroll animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,24 @@ import Hero from "./Hero";
 import About from "./About";
 import ProjectsIntro from "./ProjectsIntro";
 import Welcome from "./Welcome";
+import Lenis from '@studio-freight/lenis'
+import { useEffect } from "react";
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 export default function Home() {
+
+  useEffect(() => {
+    const lenis = new Lenis()
+
+    lenis.on('scroll', ScrollTrigger.update)
+
+    gsap.ticker.add((time)=>{
+      lenis.raf(time * 1000)
+    })
+
+    gsap.ticker.lagSmoothing(0)
+  }, []);
 
   return (
     <>
