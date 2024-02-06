1eb7696d93ba7886f7edddc4d81e49adcbe887ec
Gustavo <gumarinho090306@gmail.com>
2024-02-06 14:03:08 -0300

    Add Locomotive Scroll library and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+'use client';
 import Image from "next/image";
 import Nav from "./Nav";
 import Hero from "./Hero";
 import About from "./About";
 import ProjectsIntro from "./ProjectsIntro";
 import Welcome from "./Welcome";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    (
+      async () => {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default;
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, []);
+
   return (
     <>
       <header className="h-[84px]">
