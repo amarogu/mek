96c7a58d2a2caef8512a9ff9776b363799cbe7cc
Gustavo <gumarinho090306@gmail.com>
2024-02-06 20:16:48 -0300

    Add @gsap/react package and update dependencies

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -1,30 +1,11 @@
 import Image from 'next/image';
 import GustavoAmaro from '../../public/gustavo_amaro_image.png';
 import ArrowUpward from '../../public/arrow_upward.svg';
-import { useLayoutEffect, useRef } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function About() {
-
-    const image = useRef(null);
-
-    useLayoutEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: document.documentElement,
-                start: 'top',
-                end: '+=300px',
-                scrub: true,
-            }
-        });
-        tl.from(image.current, {clipPath: 'inset(15%)'}).to(image.current, {clipPath: 'inset(0%)'});
-    }, []);
-
     return (
         <section className='flex flex-col justify-center gap-8 h-[calc(100vh-84px)] min-h-[824px] px-8'>
-            <Image ref={image} src={GustavoAmaro} alt="Gustavo Amaro" />
+            <Image src={GustavoAmaro} alt="Gustavo Amaro" />
             <div className='flex flex-col gap-4'>
                 <div className='flex text-2xl gap-3'>
                     <p>That's me</p>
