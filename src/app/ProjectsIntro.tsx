a77e5480835faddf3fafca35aa2386ec06336fe0
Gustavo <gumarinho090306@gmail.com>
2024-02-05 22:32:29 -0300

    Add Cormorant Garamond font to ProjectsIntro component

--- src/app/ProjectsIntro.tsx
+++ src/app/ProjectsIntro.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import ArrowDownward from '../../public/arrow_downward.svg';
+import { Cormorant_Garamond } from 'next/font/google';
+
+const cormorantGaramond = Cormorant_Garamond({weight: '400', subsets: ['latin']});
 
 export default function ProjectsIntro() {
     return (
         <section className="px-8">
             <div className='border-t-2 border-text-200 pt-5 flex flex-col gap-6'>
-                <h2 className="text-6xl">Projects</h2>
+                <h2 className={`text-6xl ${cormorantGaramond.className}`}>Projects</h2>
                 <div className="flex gap-3">
                     <Image src={ArrowDownward} alt="Arrow Downward" />
                     <p className='text-2xl uppercase'>Scroll down</p>
