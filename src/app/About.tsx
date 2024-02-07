2e6a1d4c0b055262af07d7395416e442d54455c6
Gustavo <gumarinho090306@gmail.com>
2024-02-07 19:02:39 -0300

    Add scroll animation to About page image

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -2,11 +2,32 @@
 import Image from 'next/image';
 import GustavoAmaro from '../../public/gustavo_amaro_image.png';
 import ArrowUpward from '../../public/arrow_upward.svg';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function About() {
+
+    const ref = useRef<HTMLImageElement>(null);
+
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to(ref.current, {
+            clipPath: 'inset(0%)',
+            scrollTrigger: {
+                trigger: ref.current,
+                start: 'top bottom',
+                end: 'bottom bottom',
+                markers: true,
+                scrub: true,
+            }
+        })
+    }, [])
+
     return (
         <section className='flex flex-col justify-center gap-8 pb-24 px-8'>
-            <Image src={GustavoAmaro} alt="Gustavo Amaro" />
+            <Image ref={ref} src={GustavoAmaro} style={{clipPath: 'inset(30%)'}} alt="Gustavo Amaro" />
             <div className='flex flex-col gap-4'>
                 <div className='flex text-2xl gap-3'>
                     <p>That's me</p>
