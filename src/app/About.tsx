98b827d3a6b9026a6ecf2a2eb7390c319369621c
Gustavo <gumarinho090306@gmail.com>
2024-02-07 12:01:42 -0300

    Update section heights and alignments

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 
 export default function About() {
     return (
-        <section className='flex snap-center flex-col justify-center gap-8 h-screen min-h-[824px] px-8'>
+        <section className='flex snap-end flex-col justify-center gap-8 h-[calc(100vh-84px)] min-h-[824px] px-8'>
             <Image src={GustavoAmaro} alt="Gustavo Amaro" />
             <div className='flex flex-col gap-4'>
                 <div className='flex text-2xl gap-3'>
