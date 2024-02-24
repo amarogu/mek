b9a65cf84817e1ee6b02c26233b9ea029f171e07
Gustavo <gumarinho090306@gmail.com>
2024-02-24 14:39:35 -0300

    Refactoring

--- src/app/Contact.tsx
+++ src/app/Contact.tsx
@@ -1,16 +1,31 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 export default function Contact() {
 
     const ref = useRef<any>(null);
+    const getInTouch = useRef<any>(null);
+
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to(getInTouch.current, {
+            x: '-1972.12px',
+            scrollTrigger: {
+                trigger: ref.current,
+                start: 'top bottom-=200px',
+                end: 'bottom top',
+                scrub: true,
+                
+            }
+        })
+    }, [])
 
     return (
-        <section className="px-8">
+        <section className="px-8 container mx-auto">
             <div ref={ref} className="h-[200vh] w-fit">
-                <h2 className="text-[12.5rem] text-nowrap leading-none">Need to get in touch?</h2>
+                <h2 ref={getInTouch} className={`text-[12.5rem] text-nowrap leading-none`}>Need to get in touch?</h2>
             </div>
         </section>
     )
