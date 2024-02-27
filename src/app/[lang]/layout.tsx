cba9813e5a6489890c1040bb6a1f9bcb152ba599
Gustavo <gumarinho090306@gmail.com>
2024-02-27 16:25:47 -0300

    Update tsconfig.json, package.json, and package-lock.json

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import { i18n, type Locale } from "../../i18n.config";
+
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }));
+}
 
 const manrope = Manrope({subsets: ["latin"]});
 
@@ -11,11 +16,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  params
 }: Readonly<{
   children: React.ReactNode;
+  params: { lang: Locale };
 }>) {
   return (
-    <html lang="en">
+    <html lang={params.lang}>
       <body className={`${manrope.className} text-base text-text-200 bg-bg-100 antialiased`}>{children}</body>
     </html>
   );
