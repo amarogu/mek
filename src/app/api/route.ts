c479470aa362546ea7ed4cef74b46800cd5b9d0d
Gustavo <gumarinho090306@gmail.com>
2024-02-29 16:07:38 -0300

    Add SendGrid mail dependency and update route.ts

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,1 @@
-export async function GET() {
-    return new Response("Hello, world!", {
-        status: 200,
-        headers: {
-            "content-type": "text/plain",
-        }
-    });
-}
+import { NextApiRequest, NextApiResponse } from 'next';
