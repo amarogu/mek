d1a6a3fbe3a163efbe599873cbee6fcd87060a7f
Gustavo <gumarinho090306@gmail.com>
2024-03-07 18:38:42 -0300

    Add new dependencies and update Hero component

---
+++ src/app/Parallax.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { useWindowSize } from "@studio-freight/hamo";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+interface ParallaxProps {
+    className?: string;
+    children: React.ReactNode;
+    speed?: number;
+    id?: string;
+}
+
+export function Parallax({ className, children, speed = 1, id = 'parallax'}: ParallaxProps) {
+    const trigger = useRef<HTMLDivElement>(null);
+    const target = useRef<HTMLDivElement>(null);  
+    const timeline = useRef<GSAPTimeline>(); 
+    const { width: windowWidth } = useWindowSize() as { width: number };
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+    
+        const y = windowWidth * speed * 0.1;
+        const setY = gsap.quickSetter(target.current, "y", "px");
+    
+        timeline.current = gsap.timeline({
+          scrollTrigger: {
+            id: id,
+            trigger: trigger.current,
+            scrub: true, 
+            start: "top center",  
+            end: "bottom top", 
+            onUpdate: (e) => {
+              setY(e.progress * y);
+            },
+            markers: true
+          },
+        });
+    
+        return () => {
+          timeline?.current?.kill(); 
+        };
+      }, [id, speed, windowWidth]);
+  
+    return (
+      <div ref={trigger} className={className}>
+        <div ref={target}>{children}</div>
+      </div>
+    );
+  }
--- src/app/[lang]/[project]/Hero.tsx
+++ src/app/[lang]/[project]/Hero.tsx
@@ -3,7 +3,12 @@ import {type getDictionary} from "@/dictionaries";
 import { useMediaQuery } from "react-responsive";
 import UniStayTitle from '../../../../public/unistaytitle.png';
 import UniStayTitleMobile from '../../../../public/unistaytitlemobile.png';
+import Profile from '../../../../public/profile.png';
+import HomeDark from '../../../../public/homedark.png';
+import HomeLight from '../../../../public/homelight.png';
 import Image from "next/image";
+import { useLenis } from '@studio-freight/react-lenis';
+import { Parallax } from "@/app/Parallax";
 
 interface HeroProps {
     dict: Awaited<ReturnType<typeof getDictionary>>["projects"];
@@ -12,6 +17,8 @@ interface HeroProps {
 }
 
 export default function Hero({dict, menu, project}: HeroProps) {
+
+    const lenis = useLenis(({scroll}) => {});
 
     const isMd = useMediaQuery({ query: '(min-width: 768px)' });
 
@@ -45,6 +52,15 @@ export default function Hero({dict, menu, project}: HeroProps) {
                     <p className="border-b pb-2 border-text-200">{global.hero.loc}</p>
                     <p className="text-xl">{local.hero.loc}</p>
                 </div>
+            </div>
+            <div className="md:container relative flex justify-center items-center w-full md:mx-auto">
+                <Image src={HomeDark} className="w-[80%] order-1" alt="UniStay home dark" />
+                <Parallax speed={4}className="absolute -left-1/2 w-[70%] -z-10" >
+                    <Image src={HomeLight} alt="UniStay home light" />
+                </Parallax>
+                <Parallax speed={4} className="absolute w-[70%] -right-1/2 -z-10">
+                    <Image src={Profile} alt="Profile" />
+                </Parallax>
             </div>
         </section>
     )
