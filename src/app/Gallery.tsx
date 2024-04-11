4acdf4a03f7f18a04ac8f4c43431d5d8976b33a9
Gustavo <gumarinho090306@gmail.com>
2024-04-11 19:11:55 -0300

    Update focus behavior in Gallery component and add close button functionality

--- src/app/Gallery.tsx
+++ src/app/Gallery.tsx
@@ -10,45 +10,71 @@ import Img8 from '../../public/img8.jpg';
 import Img9 from '../../public/img9.jpg';
 import Img10 from '../../public/img10.jpg';
 import Img11 from '../../public/img11.jpg';
-import GsapMagnetic from './GsapMagnetic';
+import Close from '../../public/close.svg';
 import { Parallax } from './Parallax';
 import { useState } from 'react';
+import { createPortal } from 'react-dom';
+
+const imgs = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11];
 
 export default function Gallery() {
 
-    const [isFocused, setIsFocused] = useState<[boolean, number]>([false, 0]);
+    const [focus, setFocus] = useState<number>(-1);
 
-    const focus = (i: number) => {
-        setIsFocused([true, i]);
+    const handleClick = (i: number) => {
+        const header: HTMLElement | null = document.querySelector('header nav');
+        if (header) header.style.filter = 'blur(5px)';
+        const main = document.querySelector('main');
+        if (main) {main.style.filter = 'blur(10px)'};
+        setFocus(i);
+    }
+
+    const handleClose = () => {
+        const header: HTMLElement | null = document.querySelector('header nav');
+        if (header) header.style.filter = 'none';
+        const main = document.querySelector('main');
+        if (main) {main.style.filter = 'none'; main.style.height = 'auto'; main.style.overflow = 'auto'};
+        setFocus(-1);
     }
 
     return (
         <section className="px-8 pb-24 container mx-auto">
-            <div className='grid grid-cols-1'>
-                <Parallax reverse speed={4}>
-                    <div>
-                        <button onClick={e => focus(1)}>
-                            <Image className='' style={{filter: `${isFocused[0] && isFocused[1] === 1 ? 'blur(0px)' : 'blur(10px)'}`}} src={Img1} alt="Image 1" />
-                        </button>
-                    </div>
-                </Parallax>
-                <div className='flex gap-x-4'>
-                    <Parallax reverse speed={4}>
-                        <div>
-                            <button onClick={e => focus(2)}>
-                                <Image className='' style={{filter: `${isFocused[0] && isFocused[1] === 2 ? 'blur(0px)' : 'blur(10px)'}`}} src={Img2} alt="Image 2" />
+            <div className='grid'>
+                {imgs.map((img, i) => {
+                    if (focus === i) {
+                        return (
+                            <>
+                                {
+                                    createPortal(
+                                        <div className='fixed p-4 gap-4 left-0 top-0 w-full h-full flex justify-center items-center z-50 flex-col'>  
+                                            <button onClick={() => handleClick(i)}>
+                                                <Image src={img} alt='Imagem de Maria e Kalil' />
+                                            </button>
+                                            <button onClick={handleClose} className='rounded-full w-8 flex justify-center items-center h-8 bg-bg-300/75 self-start'>
+                                                <Image src={Close} alt='Fechar' width={24} height={24} className='opacity-50' />
+                                            </button>
+                                        </div>, 
+                                        document.body
+                                    )
+                                }
+                                <Parallax reverse speed={4}>
+                                    <button onClick={() => handleClick(i)}>
+                                        <Image src={img} alt='Imagem de Maria e Kalil' />
+                                    </button>
+                                </Parallax>
+                            </>
+                        )
+                    }
+                    return (
+                        <Parallax reverse speed={4}>
+                            <button onClick={() => handleClick(i)}>
+                                <Image src={img} alt='Imagem de Maria e Kalil' />
                             </button>
-                        </div>
-                    </Parallax>
-                    <Parallax reverse speed={4}>
-                        <div className='pt-4'>
-                            <button onClick={e => focus(3)}>
-                                <Image className='' style={{filter: `${isFocused[0] && isFocused[1] === 3 ? 'blur(0px)' : 'blur(10px)'}`}} src={Img3} alt="Image 3" />
-                            </button>
-                        </div>
-                    </Parallax>
-                </div>
-            </div>
+                        </Parallax>
+                    )
+                }
+                )}
+            </div>  
         </section>
     )
 }
