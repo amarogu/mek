e519a160a95b1b6f802b6fb3e0b5a2b836f018fa
Gustavo <gumarinho090306@gmail.com>
2024-04-11 19:26:43 -0300

    Update UI styles in Hero and Nav components, remove unused imports, and delete Slider, ContactField, and ProjectsIntro components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
 'use client';
-import Nav from "./Nav";
-import Hero from "./Hero";
-import About from "./About";
 import Welcome from "./Welcome";
-import Image from "next/image";
-import UniStay from '../../public/unistayplaceholder.jpeg';
-import PrettyChatPlaceholder from '../../public/prettychatplaceholder.png';
-import Projects from "./Projects";
 import { ReactLenis } from '@studio-freight/react-lenis'
-import Contact from "./Contact";
-import Footer from "./Footer";
 import { useEffect, useState } from "react";
-import Gallery from "./Gallery";
 
 export default function Home() {
 
@@ -27,15 +17,9 @@ export default function Home() {
     <ReactLenis root>
       <header className="h-[84px] relative z-20">
         <Welcome />
-        <Nav />
       </header>
       <main id="main" className="relative text-3xl overflow-x-hidden">
         <div id="spacer"></div>
-        <Hero />
-        <About />
-        <Gallery />
-        <Contact />
-        <Footer />
       </main>
     </ReactLenis>
   );
