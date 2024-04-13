7e4c8a6de477b9bd791a3c73ae3a7c88c4ad4715
Gustavo <gumarinho090306@gmail.com>
2024-04-12 22:06:21 -0300

    Refactor Slider component to include open/close functionality and update UI styles

--- src/app/Slider.tsx
+++ src/app/Slider.tsx
@@ -8,21 +8,39 @@ interface SliderProps {
 
 export default function Slider({open}: SliderProps) {
 
+    const aside = useRef<HTMLElement>(null);
+
     useGSAP(() => {
         const tl = gsap.timeline({});
-        tl.to('.i', {
+        if (open) {
+            tl.to(aside.current, {
+                height: '100%',
+                duration: 0.3,
+            }).to('.i', {
+                y: '-100%',
+                stagger: 0.1,
+            })
+        } else {
+            tl.to('.i', {
+                y: '0%',
+                stagger: 0.1,
+            }).to(aside.current, {
+                height: '0%',
+                duration: 0.3,
+            })
+        }
+    }, [open])
 
-        })
-    }, [])
+    const data = ['casal', 'festa', 'galeria', 'recados', 'presentes']
 
     return (
-        <aside className={`fixed bottom-0 flex flex-col justify-end w-full dark:bg-dark-bg-200 bg-bg-200 overflow-hidden`}>
+        <aside ref={aside} style={{height: '0%'}} className={`fixed bottom-0 flex flex-col justify-end w-full dark:bg-dark-bg-200 bg-bg-200 overflow-hidden`}>
             <ul className="flex text-3xl flex-col p-8 gap-4">
-                <li><button className="uppercase i"><span>Casal</span><div className="dark:bg-dark-bg-200 bg-bg-200 w-full block h-full"></div></button></li>
-                <li><button className="uppercase i">Festa</button></li>
-                <li><button className="uppercase i">Galeria</button></li>
-                <li><button className="uppercase i">Recados</button></li>
-                <li><button className="uppercase i">Presentes</button></li>
+                {data.map(((item, i) => {
+                    return (
+                        <li key={i}><button className="uppercase overflow-hidden relative"><span>{item}</span><div className="dark:bg-dark-bg-200 i top-0 left-0 h-9 bg-bg-200 w-full absolute"></div></button></li>
+                    )
+                }))}
             </ul>
         </aside>
     )
