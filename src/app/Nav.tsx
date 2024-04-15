840e993a5fe6ebc82ccef211a804e2c6de988364
Gustavo <gumarinho090306@gmail.com>
2024-04-15 18:27:14 -0300

    Refactor Nav component to include key prop in secondUl map function in Nav.tsx

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -40,7 +40,7 @@ export default function Nav() {
             </ul>
             <Image src={isDarkMode ? LogoDark : Logo} className="col-start-1 static z-20 md:col-start-2" alt="Maria & Kalil, com amor," />
             <ul className="md:flex hidden gap-12 text-nowrap justify-self-end font-bold">
-                {secondUl.map(i => <li><AnimatedText el={<button className="uppercase overflow-hidden h-[15px] text-[10px]">{i}</button>} animation="upper-staggering" on="hover" content={i} /></li>)}
+                {secondUl.map(i => <li key={i}><AnimatedText el={<button className="uppercase overflow-hidden h-[15px] text-[10px]">{i}</button>} animation="upper-staggering" on="hover" content={i} /></li>)}
             </ul>
             <button className="cursor-pointer static z-20 col-start-3 md:hidden flex items-center justify-end justify-self-end w-[24px] h-[14px] " onClick={() => setOpen(!open)}>
                 <div className="flex flex-col relative w-4 h-[6px]" >
