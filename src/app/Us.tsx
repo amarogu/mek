dd75d36f3fab2680df32553f1d7a897675ce078c
Gustavo <gumarinho090306@gmail.com>
2024-04-16 20:30:05 -0300

    Refactor Us component to include dynamic height calculation, update UI styles, and add scroll animation in Us.tsx

--- src/app/Us.tsx
+++ src/app/Us.tsx
@@ -1,8 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Us() {
 
-    const data = ['Um amor que', 'tinha que', 'acontecer']
+    const data = ['Um amor que', 'tinha que', 'acontecer'];
+
+    const container = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const usTitle = document.querySelectorAll('.usTitle');
@@ -14,11 +19,22 @@ export default function Us() {
         heights.forEach((height, i) => {
             hidingRects[i].setAttribute('style', `height: ${height}px`);
         })
+    }, []);
+
+    useGSAP(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.to('.hidingRect', {
+            yPercent: 100,
+            scrollTrigger: {
+                trigger: container.current,
+                start: 'top center'
+            }
+        })
     }, [])
 
     return (
         <section className="flex items-center text-[12.5vw] md:text-[9vw] xl:text-[120px] font-extrabold leading-[85%] text-center justify-center h-screen">
-            <div>
+            <div ref={container}>
                 {data.map(i => {
                     return (
                         <div key={i} className="relative">
