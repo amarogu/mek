c0914ea0432563740f14a632250c3976509b4392
Gustavo <gumarinho090306@gmail.com>
2024-04-16 14:41:01 -0300

    Refactor UI styles and update dependencies in Slider.tsx and page.tsx

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Logo from '../../public/meklogo.svg';
 import LogoDark from '../../public/meklogo_dark.svg';
 import Context from "./Context";
-import { useContext, useState } from "react";
+import { useContext, useRef, useEffect, Dispatch, SetStateAction } from "react";
 import { useSpring, animated } from "@react-spring/web";
-import Slider from "./Slider";
 import AnimatedText from "./AnimatedText";
+import { addClasses, removeClasses } from "@/lib/helpers";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const config = {
     mass: 1,
@@ -13,10 +15,11 @@ const config = {
     friction: 12
 }
 
-export default function Nav() {
+export default function Nav({open, setOpen}: {open: boolean, setOpen: Dispatch<SetStateAction<boolean>>}) {
 
     const { isDarkMode } = useContext(Context);
-    const [open, setOpen] = useState<boolean>(false);
+
+    const isMd = useMediaQuery({query: '(min-width: 768px)'});
 
     const topBar = useSpring({
         transform: open ? "rotate(45deg)" : "rotate(0deg)",
@@ -33,22 +36,47 @@ export default function Nav() {
     const firstUl = ['casal', 'festa']
     const secondUl = ['galeria', 'recados', 'presentes']
 
+    const nav = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (nav.current) {
+                if (window.scrollY > 81) {
+                    addClasses(nav.current, ['fixed', 'left-0', '-top-[104.54px]', 'bg-bg-200', 'dark:bg-dark-bg-200', 'p-8', 'z-20']);
+                    removeClasses(nav.current, ['bg-transparent']);
+                    gsap.to(nav.current, {y: '100%', ease: 'slow'});
+                    document.body.style.paddingTop = '81px';
+                } else {
+                    removeClasses(nav.current, ['fixed', 'left-0', '-top-[104.54px]', 'bg-bg-200', 'dark:bg-dark-bg-200', 'p-8', 'z-20']);
+                    addClasses(nav.current, ['bg-transparent']);
+                    gsap.to(nav.current, {y: '0%', ease: 'slow'});
+                    document.body.style.paddingTop = '0';
+                }
+            }
+        }
+
+        if (!isMd) {
+            window.addEventListener('scroll', handleScroll)
+        }
+
+        return () => {if (!isMd) window.removeEventListener('scroll', handleScroll)};
+    }, [isMd]);
+
     return (
-        <nav className="grid container grid-cols-3 mx-auto items-center justify-items-center">
+        <nav ref={nav} className="grid static z-30 container grid-cols-3 mx-auto items-center justify-items-center">
             <ul className="md:flex hidden gap-12 justify-self-start font-bold">
                 {firstUl.map(i => <li key={i}><AnimatedText animation="upper-staggering" on="hover" el={<button className="uppercase overflow-hidden h-[15px] text-[10px]"></button>} content={i}  /></li>)}
             </ul>
-            <Image src={isDarkMode ? LogoDark : Logo} className="col-start-1 static z-40 md:col-start-2" alt="Maria & Kalil, com amor," />
+            <Image src={isDarkMode ? LogoDark : Logo} className="col-start-1 md:col-start-2" alt="Maria & Kalil, com amor," />
             <ul className="md:flex hidden gap-12 text-nowrap justify-self-end font-bold">
                 {secondUl.map(i => <li key={i}><AnimatedText el={<button className="uppercase overflow-hidden h-[15px] text-[10px]">{i}</button>} animation="upper-staggering" on="hover" content={i} /></li>)}
             </ul>
-            <button className="cursor-pointer static z-40 col-start-3 md:hidden flex items-center justify-end justify-self-end w-[24px] h-[14px] " onClick={() => setOpen(!open)}>
+            <button className="cursor-pointer col-start-3 md:hidden flex items-center justify-end justify-self-end w-[24px] h-[14px] " onClick={() => setOpen(!open)}>
                 <div className="flex flex-col relative w-4 h-[6px]" >
                     <animated.span style={{...topBar}} className="w-4 h-[1px] absolute inline-block bg-text-200 dark:bg-dark-text-200"></animated.span>
                     <animated.span style={{...bottomBar}} className="w-4 h-[1px] absolute inline-block bg-text-200 dark:bg-dark-text-200"></animated.span>
                 </div>
             </button>
-            <Slider open={open} />
         </nav>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 'use client';
 import Welcome from "./Welcome";
 import { ReactLenis } from '@studio-freight/react-lenis'
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import Context from "./Context";
 import Nav from "./Nav";
 import Hero from "./Hero";
-import gsap from "gsap";
-
-function addClasses(element: HTMLElement, classes: string[]) {
-  classes.forEach(className => {
-    element.classList.add(className);
-  });
-}
-
-function removeClasses(element: HTMLElement, classes: string[]) {
-  classes.forEach(className => {
-    element.classList.remove(className);
-  });
-}
+import Slider from "./Slider";
 
 export default function Home() {
 
@@ -25,42 +13,22 @@ export default function Home() {
 
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
+  const [open, setOpen] = useState<boolean>(false);
+
   useEffect(() => {
     setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
     setIsMounted(true);
   }, []);
-
-  const header = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-      const handleScroll = () => {
-          if (header.current) {
-              if (window.scrollY > 81) {
-                  addClasses(header.current, ['fixed', '-top-[81px]', 'bg-bg-200', 'dark:bg-dark-bg-200', 'pb-8', 'z-20']);
-                  removeClasses(header.current, ['bg-bg-100', 'dark:bg-dark-bg-100']);
-                  gsap.to(header.current, {y: '100%', ease: 'slow'});
-                  document.body.style.paddingTop = '81px';
-              } else {
-                  removeClasses(header.current, ['fixed', '-top-[81px]', 'bg-bg-200', 'dark:bg-dark-bg-200', 'pb-8', 'z-20']);
-                  addClasses(header.current, ['bg-bg-100', 'dark:bg-dark-bg-100']);
-                  gsap.to(header.current, {y: '0%', ease: 'slow'});
-                  document.body.style.paddingTop = '0';
-              }
-          }
-      }
-      window.addEventListener('scroll', handleScroll)
-
-      return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
 
   if (!isMounted) return null;
 
   return (
     <ReactLenis root>
       <Context.Provider value={{isDarkMode: isDarkMode, setIsDarkMode: setIsDarkMode}}>
-        <header ref={header} className="px-8 pt-8 h-[81px] flex items-center">
+        <header className="px-8 pt-8 h-[81px] flex items-center">
           <Welcome />
-          <Nav />
+          <Nav open={open} setOpen={setOpen} />
+          <Slider open={open} />
         </header>
         <main id="main" className="overflow-x-hidden h-[500vh]">
           <div id="spacer"></div>
