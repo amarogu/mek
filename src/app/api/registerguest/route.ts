1e28c58c5ddc321db14fa8709a005bb4853ee241
Gustavo <gumarinho090306@gmail.com>
2024-05-07 18:17:02 -0300

    Refactor image references and register guest with provided parameters

--- src/app/api/registerguest/route.ts
+++ src/app/api/registerguest/route.ts
@@ -3,11 +3,11 @@ import { connectDb } from "@/lib/connect";
 import { type NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
+    const name = req.nextUrl.searchParams.get('name');
+    const gender = req.nextUrl.searchParams.get('gender');
+    const multipleGuests = req.nextUrl.searchParams.get('multipleGuests');
     try {
         await connectDb();
-        const name = req.nextUrl.searchParams.get('name');
-        const gender = req.nextUrl.searchParams.get('gender');
-        const multipleGuests = req.nextUrl.searchParams.get('multipleGuests');
         const user = new User({name, gender, multipleGuests});
         await user.save();
         return Response.json({message: 'User registered successfully', user});
