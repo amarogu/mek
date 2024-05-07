2cb8f9d6d95c258c3269414b4e33db4ef49ad35e
Gustavo <gumarinho090306@gmail.com>
2024-05-07 15:03:01 -0300

    Refactor image references and fix user not found redirect

--- src/app/[user]/Page.tsx
+++ src/app/[user]/Page.tsx
@@ -1,10 +1,10 @@
 import instance from "@/lib/axios";
 import { type User } from "@/lib/Models/User";
 import Content from "./Content";
-import { notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default async function Home({params}: {params?: {user: string}}) {
     const user = (await instance.get(`user?_id=${params?.user}`)).data.user as User;
-    if (!user) notFound();
+    if (!user) redirect('/');
     return <Content user={user} />;
 }
