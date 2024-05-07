8a872563909970281ea92694271e1b99c45dbb3d
Gustavo <gumarinho090306@gmail.com>
2024-05-07 15:07:07 -0300

    Refactor image references and remove unused code in Content.tsx

--- src/app/[user]/Content.tsx
+++ src/app/[user]/Content.tsx
@@ -1,21 +1,15 @@
 'use client';
 import Welcome from "../Welcome";
-import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
+import { ReactLenis } from '@studio-freight/react-lenis'
 import { useEffect, useRef, useState } from "react";
 import Context from "../Context";
 import Nav from "../Nav";
 import Hero from "../Hero";
 import Slider from "../Slider";
 import Us from "../Us";
-import { checkSwipe, getElScrollPos } from "@/lib/helpers";
 import { type User } from "@/lib/Models/User";
 
 export default function Content({user}: {user: User}) {
-
-  const lenis = useLenis(() => {});
-
-  const [sections, setSections] = useState<number[]>([]);
-  const [sectionIndex, setSectionIndex] = useState(0);
 
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
@@ -27,47 +21,10 @@ export default function Content({user}: {user: User}) {
   const header = useRef<HTMLElement>(null);
 
   useEffect(() => {
-    const us = {
-      el: document.getElementById('us'),
-      topPos: undefined as number | undefined,
-      bottomPos: undefined as number | undefined,
-    }
-    if (us.el) {
-      us.bottomPos = getElScrollPos(us.el, 'bottom');
-      us.topPos = getElScrollPos(us.el, 'top');
-    }
-    if (us.bottomPos && us.topPos) {
-      setSections([0, us.topPos, us.bottomPos + 3000]);
-    }
-  }, [isMounted, lenis])
-
-  useEffect(() => {
     if (spacer.current && header.current) {
       spacer.current.setAttribute('style', `height: ${header.current.clientHeight}px`);
     }
-    let touchStartedY: number;
-    let touchEndedY: number;
-    const handleTouchStart = (e: TouchEvent) => {
-      touchStartedY = e.changedTouches[0].clientY;
-    }
-    const handleTouchEnd = (e: TouchEvent) => {
-      let newIndex = sectionIndex;
-      touchEndedY = e.changedTouches[0].clientY;
-      if (checkSwipe(touchStartedY, touchEndedY) === 'up') {
-        newIndex = Math.min(newIndex + 1, sections.length - 1);
-      } else {
-        newIndex = Math.max(newIndex - 1, 0);
-      }
-      if (lenis) {lenis.scrollTo(sections[newIndex], {duration: newIndex === 2 || (checkSwipe(touchStartedY, touchEndedY) === 'down' && newIndex === 1) ? 4 : 1.5})};
-      setSectionIndex(newIndex);
-    }
-    //window.addEventListener('touchstart', handleTouchStart, {passive: false});
-    //window.addEventListener('touchend', handleTouchEnd, {passive: false});
-    return () => {
-      //window.removeEventListener('touchstart', handleTouchStart);
-      //window.removeEventListener('touchend', handleTouchEnd);
-    }
-  }, [isMounted, lenis, sectionIndex, sections]);
+  }, [isMounted]);
 
   useEffect(() => {
     let height = window.innerHeight;
