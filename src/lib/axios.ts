6bf45c3176f81a8fd00e19dc695772c01ec7337d
Gustavo <gumarinho090306@gmail.com>
2024-05-09 14:06:05 -0300

    Refactor image references and update base URL in axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,13 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: '/api',
-    timeout: 10000,
-    proxy: {
-        host: 'mariaekalil.com',
-        port: 80,
-        protocol: 'https'
-    }
+    baseURL: 'https://mariaekalil.com/api',
+    timeout: 10000
 });
 
 export default instance;
