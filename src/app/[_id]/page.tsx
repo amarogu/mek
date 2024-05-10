a37d046da2832fc9f64de6b756b3737cec0e6430
Gustavo <gumarinho090306@gmail.com>
2024-05-09 22:34:13 -0300

    chore: Refactor user page to remove unnecessary try-catch block and improve error handling

--- src/app/[_id]/page.tsx
+++ src/app/[_id]/page.tsx
@@ -1,12 +1,19 @@
-import instance from "@/lib/axios";
-import { type User } from "@/lib/Models/User";
+'use server';
+import { connectDb } from "@/lib/connect";
+import { User } from "@/lib/Models/User";
 import Content from "./Content";
 import { redirect } from "next/navigation";
 
 export default async function Home({params}: {params?: {_id: string}}) {
     let user: User | null = null;
 
-    user = (await instance.get(`/user?id=${params?._id}`)).data.user as User;
+    await connectDb();
+
+    try {
+        if (params) user = await User.findById(params._id);
+    } catch (e: any) {
+        redirect('/');
+    }
 
     if (!user) redirect('/');
 
