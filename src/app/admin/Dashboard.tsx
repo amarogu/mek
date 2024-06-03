d09b874cc552916a8f06a1e95f977d6359fd6b15
Gustavo <gumarinho090306@gmail.com>
2024-06-02 22:18:59 -0300

    Improving readability

--- src/app/admin/Dashboard.tsx
+++ src/app/admin/Dashboard.tsx
@@ -1,19 +1,16 @@
 'use client';
-import { IGift } from "@/lib/Models/Gift";
-import { IGroup } from "@/lib/Models/Group";
-import { IMsg } from "@/lib/Models/Msg";
-import { IUser } from "@/lib/Models/User";
-import { HydratedDocument, MergeType } from "mongoose";
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
+import { IAdminData } from "@/lib/actions/getAdminData";
 
-export default function Dashboard({session, data}: {session: Session, data?: {giftsGiven: HydratedDocument<MergeType<IUser | IGroup, {giftsGiven: IGift[]}>>[], msgs: HydratedDocument<MergeType<IUser | IGroup, {msgs: IMsg[]}>>[]}}) {
+export default function Dashboard({session, data}: {session: Session, data?: IAdminData}) {
     
     return (
         <div>
             <h1>Dashboard</h1>
             <p>Welcome {session?.user?.name}</p>
             <button onClick={() => signOut()}>Sign out</button>
+            <p>{data?.giftsGiven[0].giftsGiven[0].title}</p>
         </div>
     )
 }
--- src/lib/actions/getAdminData.ts
+++ src/lib/actions/getAdminData.ts
@@ -1,5 +1,15 @@
+import { HydratedDocument, MergeType } from "mongoose";
 import { getGiftsGiven } from "./getGiftsGiven";
 import { getMessages } from "./getMessages";
+import { IUser } from "../Models/User";
+import { IGroup } from "../Models/Group";
+import { IGift } from "../Models/Gift";
+import { IMsg } from "../Models/Msg";
+
+export interface IAdminData {
+    giftsGiven: HydratedDocument<MergeType<IUser | IGroup, {giftsGiven: IGift[]}>>[];
+    msgs: HydratedDocument<MergeType<IUser | IGroup, {msgs: IMsg[]}>>[];
+}
 
 export async function getAdminData() {
     const giftsGiven = await getGiftsGiven();
