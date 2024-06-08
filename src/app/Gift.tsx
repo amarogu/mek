7ef5319e85d325d96d186355b5423954be8ab94c
Gustavo <gumarinho090306@gmail.com>
2024-06-08 16:08:00 -0300

    Adding UI representation of a sold out gift

--- src/app/Gift.tsx
+++ src/app/Gift.tsx
@@ -1,7 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import { LeanDocument } from "@/lib/helpers";
 import { IGift, IUser, IGroup } from "@/lib/Models/Interfaces";
@@ -52,11 +52,18 @@ export default function Gift({gift, item}: {gift: LeanDocument<IGift>, item?: Le
             });
         }
 
-    }, [hovering])
+    }, [hovering]);
+
+    useEffect(() => {
+        if (gift.soldOut) setHovering(true);
+    }, [])
 
     return (
-        <Link href={item ? 'users' in item ? `/group/${item._id}/gift/${gift._id}` : `/guest/${item._id}/gift/${gift._id}` : `/gift/${gift._id}`}>
-            <div ref={container} onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)} className="w-full cursor-pointer relative overflow-y-hidden bg-bg-200 dark:bg-dark-bg-200 p-4 flex gap-12 flex-col">
+        <Link aria-disabled={gift.soldOut} className={`${gift.soldOut ? 'pointer-events-none relative' : ''}`} href={item ? 'users' in item ? `/group/${item._id}/gift/${gift._id}` : `/guest/${item._id}/gift/${gift._id}` : `/gift/${gift._id}`}>
+            <div className={`${gift.soldOut ? 'absolute z-20 flex items-center justify-center w-full h-full' : 'hidden'}`}>
+                <p className="uppercase text-2xl font-bold p-4 bg-primary-200 dark:bg-dark-primary-100 border border-primary-300 dark:border-dark-primary-300">Esgotado</p>
+            </div>
+            <div ref={container} onMouseEnter={() => setHovering(true)} onMouseLeave={() => setHovering(false)} className={`w-full cursor-pointer relative overflow-y-hidden bg-bg-200 dark:bg-dark-bg-200 p-4 flex gap-12 flex-col ${gift.soldOut ? 'opacity-25' : ''}`}>
                 <div ref={upperContainer} style={{transform: 'translateY(-16px) translateY(-100%)'}} className="flex flex-col gap-2">
                     <h3 className="text-xl font-semibold">{gift.title}</h3>
                     <h4 className="text-xl">{gift.description}</h4>
