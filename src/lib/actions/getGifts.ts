8e2a014aeb41419793cb8865fa17c1947dd4f57f
Gustavo <gumarinho090306@gmail.com>
2024-06-10 19:43:12 -0300

    Type correction

--- src/lib/actions/getGifts.ts
+++ src/lib/actions/getGifts.ts
@@ -1,10 +1,11 @@
 import { Model } from "mongoose";
 import { IGift } from "../Models/Interfaces";
+import { LeanDocument } from "../helpers";
 
 export async function getGifts({Gift}: {Gift: Model<IGift>}) {
     const gifts = await Gift.find();
     if (gifts) {
-        const parsedGifts = gifts.map(gift => gift.toObject<IGift & {_id: string}>({flattenObjectIds: true}))
+        const parsedGifts: LeanDocument<IGift>[] = gifts.map(gift => gift.toObject({flattenObjectIds: true}))
         return parsedGifts;
     }
     return null;
