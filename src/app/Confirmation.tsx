2b5f99a029f7cd58cba26c76aaa6f987afe764e1
Gustavo <gumarinho090306@gmail.com>
2024-06-11 18:33:23 -0300

    Managed to make the zoom in / zoom out text animation

--- src/app/Confirmation.tsx
+++ src/app/Confirmation.tsx
@@ -1,6 +1,6 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { useRef } from "react";
+import { CSSProperties, useRef } from "react";
 
 export default function Confirmation() {
 
@@ -23,19 +23,19 @@ export default function Confirmation() {
                 markers: true
             }
         }).to(textRefs[0].current, {
-            scale: 25,
+            '--progress': 1,
             ease: 'power1.in'
         }).to(textRefs[1].current, {
-            scale: 25,
+            '--progress': 1,
             ease: 'power1.in'
-        });
+        })
     })
 
     return (
         <section ref={container} className="font-extrabold leading-[85%]">
             <div className="h-screen relative p-8">
                 {
-                    text.map((t, i) => <h2 key={i} ref={textRefs[i]} className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ${i === 1 ? 'text-dark-text-100 dark:text-text-100 scale-0 text-[100vh]' : 'scale-[0.12] text-[50vh]'}`}>{t}</h2>)
+                    text.map((t, i) => <h2 key={i} style={{'--progress': i === 1 ? '0' : '0.0048'} as CSSProperties} ref={textRefs[i]} className={`absolute text-[104.17vw] md:text-[75vw] xl:text-[1000px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-[calc(var(--progress)*25)] origin-[calc(50%+var(--progress)*1%)_center] ${i === 1 ? 'text-dark-text-100 dark:text-text-100' : ''}`}>{t}</h2>)
                 }
             </div>
         </section>
