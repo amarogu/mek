46d6d1e1eb1a15a4a36c790f562911f881909dd7
Gustavo <gumarinho090306@gmail.com>
2024-06-11 19:07:55 -0300

    Added placeholder for another section

--- src/app/Confirmation.tsx
+++ src/app/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { addClasses, removeClasses } from "@/lib/helpers";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { CSSProperties, useRef } from "react";
@@ -17,10 +18,20 @@ export default function Confirmation() {
             scrollTrigger: {
                 trigger: container.current,
                 start: 'top top',
-                end: 'bottom+=1500% top',
+                end: 'bottom top',
                 scrub: true,
                 pin: true,
-                markers: true
+                pinSpacing: false,
+                onLeave: () => {
+                    textRefs.forEach(ref => {
+                        if (ref.current) addClasses(ref.current, ['hidden']);
+                    })
+                },
+                onEnterBack: () => {
+                    textRefs.forEach(ref => {
+                        if (ref.current) removeClasses(ref.current, ['hidden']);
+                    })
+                }
             }
         }).to(textRefs[0].current, {
             '--progress': 1,
@@ -32,11 +43,16 @@ export default function Confirmation() {
     })
 
     return (
-        <section ref={container} className="font-extrabold leading-[85%]">
-            <div className="h-screen relative p-8">
-                {
-                    text.map((t, i) => <h2 key={i} style={{'--progress': i === 1 ? '0' : '0.0048'} as CSSProperties} ref={textRefs[i]} className={`absolute text-[104.17vw] md:text-[75vw] xl:text-[1000px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-[calc(var(--progress)*25)] origin-[calc(50%+var(--progress)*1%)_center] ${i === 1 ? 'text-dark-text-100 dark:text-text-100' : ''}`}>{t}</h2>)
-                }
+        <section>
+            <div ref={container} className="font-extrabold h-[1500vh] leading-[85%]">
+                <div className="h-screen relative p-8">
+                    {
+                        text.map((t, i) => <h2 key={i} style={{'--progress': i === 1 ? '0' : '0.0048'} as CSSProperties} ref={textRefs[i]} className={`absolute text-[104.17vw] md:text-[75vw] xl:text-[1000px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 scale-[calc(var(--progress)*25)] origin-[calc(50%+var(--progress)*1%)_center] ${i === 1 ? 'text-dark-text-100 dark:text-text-100' : ''}`}>{t}</h2>)
+                    }
+                </div>
+            </div>
+            <div className="h-[200vh] bg-dark-text-100 dark:bg-text-100">
+
             </div>
         </section>
     )
