c136761fca903101d150b620354f8f8e256a9c56
Gustavo <gumarinho090306@gmail.com>
2024-07-01 17:00:16 -0300

    Dropwdown is now fully functional.

--- src/app/ConfirmationForm.tsx
+++ src/app/ConfirmationForm.tsx
@@ -1,14 +1,11 @@
-import { Fragment, useContext, useState } from "react"
+import { Fragment, useContext } from "react"
 import Context from "./Context"
-import ArrowDark from '../../public/keyboard_arrow_down_dark.svg';
-import Arrow from '../../public/keyboard_arrow_down.svg';
-import ThemeImage from "./ThemeImage";
-import Image from "next/image";
 import Dropdown from "./Dropdown";
+import instance from "@/lib/axios";
 
 export default function ConfirmationForm() {
 
-    const { item, isDarkMode } = useContext(Context);
+    const { item } = useContext(Context);
 
     if (!item || !('users' in item)) {
         return null;
@@ -16,9 +13,16 @@ export default function ConfirmationForm() {
 
     const sortedItems = item.users.toSorted((a, b) => b.name.length - a.name.length);
 
-    const initialConfirmedUsers = sortedItems.map(u => u.confirmed);
-
-    const [users, setUsers] = useState(sortedItems);
+    const handleConfirmation = async (option: boolean, _id: string) => {
+        const res = await instance.post('/confirm', {
+            confirmed: option, _id
+        });
+        if (res.data.message === 'User confirmed successfully') {
+            return res.data.message;
+        } else {
+            return '';
+        }
+    }
 
     return (
         <div className="flex uppercase flex-col gap-7">
@@ -28,11 +32,11 @@ export default function ConfirmationForm() {
             </div>
             <div className="grid relative gap-y-3 grid-cols-[minmax(0,_1fr)_auto]">
                 {
-                    users.map((u, i) => {
+                    sortedItems.map((u, i) => {
                         return (
                             <Fragment key={i}>
                                 <p className="">{u.name}</p>
-                                <Dropdown style={{zIndex: users.length - i}} text={u.confirmed ? 'Sim' : 'Não'} options={['Sim', 'Não']} alt={`Lista de opções de confirmação ${u.gender === 'female' ? 'da convidada' : 'do convidado'}`} />
+                                <Dropdown _id={u._id} action={handleConfirmation} style={{zIndex: sortedItems.length - i}} text={u.confirmed ? 'Sim' : 'Não'} options={['Sim', 'Não']} alt={`Lista de opções de confirmação ${u.gender === 'female' ? 'da convidada' : 'do convidado'}`} />
                             </Fragment>
                         )
                     })
--- src/app/Dropdown.tsx
+++ src/app/Dropdown.tsx
@@ -1,13 +1,13 @@
 import ThemeImage from "./ThemeImage";
 import ArrowDark from '../../public/keyboard_arrow_down_dark.svg';
 import Arrow from '../../public/keyboard_arrow_down.svg';
-import { CSSProperties, Fragment, useRef, useState } from "react";
+import { CSSProperties, Fragment, MouseEvent, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { CustomEase } from "gsap/CustomEase";
 import Divider from "./Divider";
 
-export default function Dropdown({text, alt, options, className, style}: {text: string, alt: string, options: string[], className?: string, style?: CSSProperties}) {
+export default function Dropdown({text, alt, options, className, style, action, _id}: {text: string, alt: string, options: string[], className?: string, style?: CSSProperties, action?: (option: boolean, _id: string) => Promise<string>, _id?: string}) {
 
     const [open, setOpen] = useState(false);
 
@@ -17,7 +17,9 @@ export default function Dropdown({text, alt, options, className, style}: {text: 
 
     const indicator = useRef<HTMLPictureElement>(null);
 
-    useGSAP(() => {
+    const [labelText, setLabelText] = useState('');
+
+    const {contextSafe} = useGSAP(() => {
         gsap.registerPlugin(CustomEase);
 
         gsap.to(dropdown.current, {
@@ -25,22 +27,48 @@ export default function Dropdown({text, alt, options, className, style}: {text: 
             duration: 0.2
         });
 
-        gsap.to(label.current, {
+        gsap.to([label.current, indicator.current], {
             opacity: open ? 0.5 : 1,
             duration: 0.2
         });
     }, [open]);
 
+    useGSAP(() => {
+        gsap.to([label.current, indicator.current], {
+            opacity: 1,
+            duration: 0.2
+        });
+    }, [labelText])
+
+    const handleClick = contextSafe(async (e: MouseEvent<HTMLParagraphElement, globalThis.MouseEvent>) => {
+        if (action && _id) {
+            const option = e.currentTarget.textContent === 'Sim';
+            const res = await action(option, _id);
+            if (res === 'User confirmed successfully') {
+                setOpen(false);
+                gsap.to([label.current, indicator.current], {
+                    opacity: 0,
+                    duration: 0.2,
+                    onComplete: () => {
+                        setLabelText(option ? 'Sim' : 'Não');
+                    }
+                })
+            }
+        }
+    });
+
     return (
         <button style={style} onBlur={() => {setOpen(false)}} onClick={() => {setOpen(!open)}} className={`flex text-left uppercase font-normal relative gap-4 justify-between text-xl items-center ${className}`}>
-            <span ref={label}>{text}</span>
+            <span ref={label}>{labelText ? labelText : text}</span>
             <ThemeImage ref={indicator} srcDark={ArrowDark} srcLight={Arrow} alt={alt} />
             <div ref={dropdown} style={{transform: 'translateY(105%) scale(0)'}} className="absolute rounded-lg w-32 gap-4 text-base origin-top-right flex flex-col bg-bg-200/75 dark:bg-dark-bg-200/75 backdrop-blur-md right-0 bottom-0 py-4">
                 {
                     options.map((o, i) => {
                         return (
-                            <Fragment>
-                                <p className="px-4">{o}</p>
+                            <Fragment key={i}>
+                                <p onClick={(e) => {
+                                    handleClick(e);
+                                }} className="px-4">{o}</p>
                                 {
                                     i < options.length - 1 && i % 2 === 0 ? <Divider /> : null
                                 }
