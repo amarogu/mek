c25619d92f2a70cd65304c95061dfab7893d8aa5
Gustavo <gumarinho090306@gmail.com>
2024-07-02 14:27:11 -0300

    Refactoring Confirmation.tsx to comport singular guests

--- src/app/Confirmation.tsx
+++ src/app/Confirmation.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
-import { useEffect, useRef } from "react";
+import { MouseEvent, useContext, useEffect, useRef, useState } from "react";
 import Img from '../../public/img4_confirmation.png';
 import ConfirmationForm from "./ConfirmationForm";
+import Context from "./Context";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function Confirmation({id}: {id?: string}) {
     const sectionRef = useRef(null);
@@ -10,16 +13,63 @@ export default function Confirmation({id}: {id?: string}) {
 
     const formRef = useRef<HTMLDivElement | null>(null);
 
+    const { item } = useContext(Context);
+
+    if (!item) {
+        return null;
+    }
+
+    const isNotGroup = !('users' in item);
+
+    const xTo = useRef<gsap.QuickToFunc | null>(null);
+    const yTo = useRef<gsap.QuickToFunc | null>(null);
+
+    const btnRef = useRef<HTMLButtonElement>(null);
+    const parentRef = useRef<HTMLDivElement>(null);
+
+    const { contextSafe } = useGSAP(() => {
+        xTo.current = gsap.quickTo(btnRef.current, 'left', {duration: 0.2});
+        yTo.current = gsap.quickTo(btnRef.current, 'top', {duration: 0.2});
+    });
+
+    const handleMouseLeave = contextSafe(() => {
+        if (btnRef.current) {
+            gsap.to(btnRef.current, {
+                scale:  0,
+                duration:  0.5,  
+            })
+        }
+    });
+
+    const handleMouseMove = contextSafe((e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+        if (btnRef.current && xTo.current && yTo.current && parentRef.current) {
+            if (!parentRef.current.contains(e.target as Node)) {
+                return;
+            }
+
+            const {clientX, clientY} = e;
+            const {x, y} = parentRef.current.getBoundingClientRect();
+            const targetX = clientX - x;
+            const targetY = clientY - y;
+            xTo.current(targetX);
+            yTo.current(targetY);
+            gsap.to(btnRef.current, {
+                scale:  1,
+                duration:  0.5,  
+            });
+        }
+    });
+
     useEffect(() => {
         if (titleRef.current && formRef.current) {
             const width = titleRef.current.clientWidth;
             formRef.current.setAttribute('style', `width: ${width}px`);
         }
-    })
+    });
 
     return (
         <section id={id} ref={sectionRef} className="p-8 pt-20 pb-28">
-            <div className="text-[12.5vw] flex flex-col gap-16 items-center md:text-[9vw] xl:text-[120px] container mx-auto font-extrabold leading-[85%]">
+            <div ref={parentRef} onMouseMove={isNotGroup ? (e) => { handleMouseMove(e) } : () => {}} onMouseLeave={isNotGroup ? handleMouseLeave : () => {}} className={`text-[12.5vw] relative flex flex-col gap-16 items-center md:text-[9vw] xl:text-[120px] container mx-auto font-extrabold leading-[85%] ${isNotGroup ? 'cursor-pointer' : ''}`}>
                 <h2 ref={titleRef} className="flex flex-col">
                     <p className="ml-16">Confirmar</p>
                     <p className="flex gap-4 items-center">
@@ -31,6 +81,9 @@ export default function Confirmation({id}: {id?: string}) {
                 <div ref={formRef} className="text-3xl w-full">
                     <ConfirmationForm />
                 </div>
+                <button style={{transform: 'scale(0)'}} ref={btnRef} className="absolute">
+                    Click here
+                </button>
             </div>
         </section>
     )
